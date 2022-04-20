dabcce2d52f3ee6bfb4a220934a0e7838d7666fa
wmswm <wmswm@211.226.214.35>
2022-04-20 22:05:02 +0900

    평균값

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -8,7 +8,7 @@ public class Ex05 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		double[]num = new double[5];
+		double[] num = new double[5];
 		
 		num[0] = sc.nextDouble();
 		num[1] = sc.nextDouble();
@@ -16,17 +16,13 @@ public class Ex05 {
 		num[3] = sc.nextDouble();
 		num[4] = sc.nextDouble();
 		
-		double avr = 0;
 		int pcs = 0;
+		double avr = 0;
 		
 		for(int i=0; i<num.length; i++) {
 			pcs++;
 			avr += num[i];
-		}
-		System.out.println("평균은"+avr/pcs+"입니다.");
-		
-		
-		
+		}System.out.println("평균은"+avr/pcs+"입니다");
 		
 		
 		
