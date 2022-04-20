40d2f140c8a2d0561830074f06b095a6c4f6a82a
wmswm <wmswm@211.226.214.35>
2022-04-20 18:01:15 +0900

    더블

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		double[] dArray = new double[3];
+		dArray[0] = 6.7;
+		dArray[1] = 3.3;
+		dArray[2] = 1.2;
+		
+		for(int i=0; i<dArray.length; i++) {
+			System.out.println(dArray[i]);
+		}
+
+	}
+
+}
